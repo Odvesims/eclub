a4bfa2171be3e5c869dff7c7fa8841fbb6ce003c
Oscar D. Valdez <odvesims@gmail.com>
2015-03-20 15:26:50 -0400

    initial commit

--- app/controllers/deboxionales_controller.rb
+++ app/controllers/deboxionales_controller.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'json'
 require 'net/http'
+require 'date'
  
 class DeboxionalesController < ActionController::Base 
   respond_to :json 
@@ -24,42 +25,17 @@ class DeboxionalesController < ActionController::Base
 		JSON.generate(el_jason) 
 		render :text => JSON.generate(el_jason)
 	end
-	
-	def update
-		dia = params[:dia].to_i
-		year = params[:year]
-		day = dia + 1
-		puts day;
-		deboxionales = Deboxional.where("dia > #{dia.to_i + 1} AND anio = '#{year}'").all
-			el_jason = Array.new
-			deboxionales.each do |deb|	
-				hash = {}
-				hash["id"] = deb.id	
-				hash["fecha_dia"] = deb.fecha_dia
-				hash["titulo"] = deb.titulo
-				hash["versiculo"] = deb.versiculo
-				hash["cita"] = deb.cita
-				hash["cuerpo"] = deb.cuerpo
-				hash["autor"] = deb.autor
-				hash["dia"] = deb.dia
-				hash["anio"] = deb.anio
-				hash["fecha"] = deb.fecha
-				el_jason.push(hash)
-			end
-		JSON.generate(el_jason) 
-		render :text => JSON.generate(el_jason)	
-	end
   
 	def show
-		dia = params[:dia].to_i
-		year = params[:year]
+		dia = Date.today.yday
+		year = Date.today.year.to_s
 		configuraciones = Configuracione.first	
 		if dia > 0	
 			if configuraciones.modalidad == "anual"
-				deboxionales = Deboxional.where("dia > #{dia} AND anio = '#{year}'").all
+				deboxionales = Deboxional.where("anio = '#{year}'").all
 			elsif configuraciones.modalidad == "semanal"
 				deboxionales = Deboxional.where("dia >= #{dia}  AND dia <= #{dia} + 7 AND anio = '#{year}'").all
-			elsif configuraciones.modalidad == "uno"
+			elsif configuraciones.modalidad == "diario"
 				deboxionales = Deboxional.where("dia = #{dia} AND anio = '#{year}'").all
 			end
 		else	
