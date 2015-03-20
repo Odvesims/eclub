f53a06789dfc7c673101812c14a0a4dbd874ed38
Oscar D. Valdez <odvesims@gmail.com>
2015-03-20 15:31:37 -0400

    initial commit

--- app/controllers/deboxionales_controller.rb
+++ app/controllers/deboxionales_controller.rb
@@ -6,8 +6,27 @@ require 'date'
 class DeboxionalesController < ActionController::Base 
   respond_to :json 
 	def index
-		deboxionales = Deboxional.all
-			el_jason = Array.new
+		dia = Date.today.yday
+		year = Date.today.year.to_s
+		configuraciones = Configuracione.first	
+		if dia > 0	
+			if configuraciones.modalidad == "anual"
+				deboxionales = Deboxional.where("anio = '#{year}'").all
+			elsif configuraciones.modalidad == "semanal"
+				deboxionales = Deboxional.where("dia >= #{dia}  AND dia <= #{dia} + 7 AND anio = '#{year}'").all
+			elsif configuraciones.modalidad == "diario"
+				deboxionales = Deboxional.where("dia = #{dia} AND anio = '#{year}'").all
+			end
+		else	
+			deboxionales = Deboxional.all
+		end
+		
+		el_jason = Array.new
+		
+		if deboxionales == nil	
+			hash = [[[]]]
+			el_jason.push(hash)
+		else
 			deboxionales.each do |deb|	
 				hash = {}
 				hash["id"] = deb.id	
@@ -22,6 +41,7 @@ class DeboxionalesController < ActionController::Base
 				hash["fecha"] = deb.fecha
 				el_jason.push(hash)
 			end
+		end
 		JSON.generate(el_jason) 
 		render :text => JSON.generate(el_jason)
 	end
