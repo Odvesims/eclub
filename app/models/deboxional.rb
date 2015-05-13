dfa43883881f6058217484265bba2f6818e7159a
Oscar D. Valdez <odvesims@gmail.com>
2015-05-13 16:51:31 -0400

    commit110515

--- app/models/deboxional.rb
+++
@@ -1,5 +0,0 @@
-class Deboxional < ActiveRecord::Base
-	default_scope order: 'id'   
-	attr_accessible :id, :fecha, :dia_fecha, :titulo, :versiculo, :cuerpo, :autor, :dia, :anio, :cita
-	self.table_name = "deboxionales"  
-end
