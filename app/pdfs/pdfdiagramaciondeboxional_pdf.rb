4337d8c128020de121e72b172d968ea24e5d0db7
Oscar Valdez <odvesims@gmail.com>
2017-12-28 10:12:57 -0400

    commit

--- app/pdfs/pdfdiagramaciondeboxional_pdf.rb
+++ app/pdfs/pdfdiagramaciondeboxional_pdf.rb
@@ -47,11 +47,7 @@ class PdfdiagramaciondeboxionalPdf < Prawn::Document
 		#move_cursor_to 582
 		text "\n", size: 11, align: :center
 		font Rails.root.join("app/assets/fonts/Century-Gothic-Bold.ttf")
-		if deboxional.titulo.length > 36
-			text deboxional.titulo, size: 16, align: :left
-		else
-			text deboxional.titulo, size: 18, align: :left
-		end
+		text deboxional.titulo, size: 18, align: :left, :width: 330
 		font Rails.root.join("app/assets/fonts/Century-Gothic.ttf")
 		text deboxional.autor, size: 10, align: :left
 		font Rails.root.join("app/assets/fonts/CaviarDreams_Bold.ttf")
