395fb892679afcbcb15cfee88abbe41cdc306e44
Oscar Valdez <odvesims@gmail.com>
2017-12-28 11:39:14 -0400

    commit

--- app/pdfs/pdfdiagramaciondeboxional_pdf.rb
+++ app/pdfs/pdfdiagramaciondeboxional_pdf.rb
@@ -51,17 +51,17 @@ class PdfdiagramaciondeboxionalPdf < Prawn::Document
 		font Rails.root.join("app/assets/fonts/Century-Gothic.ttf")
 		image "app/assets/images/perfil.png", :at => [1,710], align: :center, :width => 25, :height => 25
 		font Rails.root.join("app/assets/fonts/CaviarDreams_Bold.ttf")
-		text_box(deboxional.autor, {size: 10, overflow: 'truncate', width: 300, align: :left, at: [25,695]})
+		text_box(deboxional.autor, {size: 10, overflow: 'truncate', width: 300, align: :left, at: [27,695]})
 		fechaDia = deboxional.fecha_dia.split(",")
 		text_box(fechaDia[0], {size: 14, overflow: 'truncate', width: 130, align: :center, at: [340,735]})
 		text_box(fechaDia[1], {size: 18, overflow: 'truncate', width: 130, align: :center, at: [340,720]})
 		font Rails.root.join("app/assets/fonts/CaviarDreams_Italic.ttf")
-		text_box(deboxional.versiculo + ' ' + deboxional.cita, {size: 11, overflow: 'truncate', width: 130, align: :left, at: [340,680]})
+		text_box(deboxional.versiculo + ' ' + deboxional.cita, {size: 11, overflow: 'truncate', width: 130, align: :left, at: [340,675]})
 		font Rails.root.join("app/assets/fonts/Century-Gothic.ttf")
 		if deboxional.cuerpo.length > 2000
-			text_box(deboxional.cuerpo, {size: 10, overflow: 'truncate', width: 330, align: :justify, at: [1,680]})
+			text_box(deboxional.cuerpo, {size: 10, overflow: 'truncate', width: 330, align: :justify, at: [1,675]})
 		else
-			text_box(deboxional.cuerpo, {size: 12, overflow: 'truncate', width: 330, align: :justify, at: [1,680]})
+			text_box(deboxional.cuerpo, {size: 12, overflow: 'truncate', width: 330, align: :justify, at: [1,675]})
 		end
 		font Rails.root.join("app/assets/fonts/Century-Gothic.ttf")
 		text "\n", size: 11, align: :center
