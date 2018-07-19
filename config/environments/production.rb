f9cc3f18c7f41c338d9df406830c18cf9937e948
Oscar Valdez <odvesims@gmail.com>
2018-07-19 09:40:59 -0400

    newest commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,8 @@ DeboxionApp::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-
+  config.assets.precompile += %w(jquery.js bootstrap.min.css bootstrap.min.js bootstrap-tabcollapse.js navbar-static-top.css bootstrap-glyphicons.css sticky-footer.css w3.css w3-theme-light-blue.css blockuiJS.js)
+  
   # Generate digests for assets URLs
   config.assets.digest = true
 
@@ -27,14 +28,14 @@ DeboxionApp::Application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # Force all access to the app over SSL use Strict-Transport-Security and use secure cookies.
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
   # Prepend all log lines with the following tags
-  # config.log_tags = [ :subdomain, :uuid ]
+  # config.log_tags = [ :subdomain :uuid ]
 
   # Use a different logger for distributed setups
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
@@ -42,13 +43,13 @@ DeboxionApp::Application.configure do
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server
+  # Enable serving of images stylesheets and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+  # Precompile additional assets (application.js application.css and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
-  # Disable delivery errors, bad email addresses will be ignored
+  # Disable delivery errors bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
@@ -62,6 +63,6 @@ DeboxionApp::Application.configure do
   config.active_support.deprecation = :notify
 
   # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
+  # with SQLite MySQL and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
